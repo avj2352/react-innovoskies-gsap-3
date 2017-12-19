9399502a6745ddc51b711acac1cab7c460162a87
Pramod Jingade <avj2352@gmail.com>
2017-12-19 09:33:06 +0530

    Added SCSS

--- src/components/tours-section/tours-section.jsx
+++ src/components/tours-section/tours-section.jsx
@@ -12,13 +12,27 @@ class ToursSection extends Component {
                 {/* Simple row */}
                 <div className="row">
                     <div className="col-1-of-3">
-                        Col 1 of 3
+                    {/* Card Component */}
+                        <div className="card">
+                            {/* Front side of the card */}
+                            <div className="card__side card__side--front">
+                                FRONT SIDE OF THE CARD
+                            </div>
+                            {/* Back side of the card */}
+                            <div className="card__side card__side--back">
+                                BACK SIDE OF THE CARD
+                            </div>
+                        </div>
                     </div>
                     <div className="col-1-of-3">
-                        Col 1 of 3
+                        <div className="card">
+                        Rotating Card
+                        </div>
                     </div>
                     <div className="col-1-of-3">
-                        Col 1 of 3
+                        <div className="card">
+                        Rotating Card
+                        </div>
                     </div>
                 </div>
             </section>
